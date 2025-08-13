6c4b6bf7bdebf6d0e225e55d0233a4ceec629b77
Arturo Lopez <alopezm41094@gmail.com>
2025-08-13 04:53:44 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <header class="rel

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -3,16 +3,18 @@ import { Logo } from '@/components/logo';
 export default function HeroSection() {
   return (
     <section className="relative h-screen w-full overflow-hidden">
-      <video
-        className="absolute left-1/2 top-1/2 h-auto w-auto min-h-full min-w-full -translate-x-1/2 -translate-y-1/2 object-cover"
-        src="https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4"
-        autoPlay
-        loop
-        muted
-        playsInline
-      />
-      <div className="absolute inset-0 bg-black/60" />
-      
+      <div style={{ padding: '56.25% 0 0 0', position: 'relative' }}>
+        <iframe
+ src="https://player.vimeo.com/video/1049355310?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+        src="https://player.vimeo.com/video/1049355310?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&autoplay=1&muted=1&loop=1"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+ style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+ title="Reel Harbol 2025"
+        ></iframe>
+      </div>
+      <script src="https://player.vimeo.com/api/player.js"></script>
+      <div className="absolute inset-0 bg-black/60" /> {/* Overlay div */}
       <header className="relative z-10 p-4 sm:p-8">
         <div className="flex items-center justify-between">
             <Logo className="text-3xl" />
